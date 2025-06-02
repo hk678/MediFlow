478d40a8133418890316deb4fb4a9632759e6796
KHS <1213ss@naver.com>
2025-06-02 10:00:54 +0900

    프로젝트 구조 초기 세팅

--- backend/src/main/java/kr/bigdata/web/MediFlowApplication.java
+++ backend/src/main/java/kr/bigdata/web/MediFlowApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeepGoingApplication {
+public class MediFlowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeepGoingApplication.class, args);
+		SpringApplication.run(MediFlowApplication.class, args);
 	}
 
 }
