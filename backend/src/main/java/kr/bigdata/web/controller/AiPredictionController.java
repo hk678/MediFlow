fa1fca397a44e4d8092ac52c5483a2d803ec2033
Kahlen <riconrica87@gmail.com>
2025-06-10 12:10:52 +0900

    AiPredictionController 충돌 해결 및 코드 수정

--- backend/src/main/java/kr/bigdata/web/controller/AiPredictionController.java
+++ backend/src/main/java/kr/bigdata/web/controller/AiPredictionController.java
@@ -4,14 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.bigdata.web.dto.AiPredictionResponseDto;
-import kr.bigdata.web.dto.PatientRequest;
 import kr.bigdata.web.entity.AiPrediction;
-import kr.bigdata.web.entity.EmergencyVisit;
 import kr.bigdata.web.service.AiPredictionService;
 
 @RestController
@@ -39,6 +36,7 @@ public class AiPredictionController {
     }
 
     // 예측 결과 조회
+    @GetMapping("/{visitId}/predictions")
     public AiPredictionResponseDto getPrediction(@PathVariable String visitId) {
         AiPrediction prediction = aiPredictionService.getPredictionByVisitId(visitId);
         return aiPredictionService.toDto(prediction);
