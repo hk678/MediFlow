4cc40fd799aaf6d3b1acc51ece62f0179555eb6b
SEIL <dltpdlf826@naver.com>
2025-06-10 10:07:43 +0900

    6월10일 오전

--- frontend/src/Components/UserUpdate.jsx
+++ frontend/src/Components/UserUpdate.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { X } from "lucide-react";
 import axios from "axios"; // ✅ axios import
-import '../Style/UserUpdate.css';
+import '../Style/Userupdate.css';
 
 
 export default function UserUpdate({ onClose }) {
